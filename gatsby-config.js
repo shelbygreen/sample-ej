8cdff1e6a045790c83da17c129a3200e54a9f89c
shelbygreen <53214352+shelbygreen@users.noreply.github.com>
2021-01-29 12:12:27 -0500

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,62 @@
+const config = require('./config/meta')
+
+module.exports = {
+  siteMetadata: {
+    title: `Texas Environmental Justice Explorer`,
+    description: `A simple tool to explore the EJ hotspots in Texas.`,
+    author: `Climate Cabinet`,
+    mapboxToken: `pk.eyJ1IjoicGFzaWgiLCJhIjoiY2pybzJqdTVjMHJzeDQ0bW80aGdzaXV3ayJ9.yxD8Nqu7FLnf8-lBo7F1zQ`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/data`,
+      },
+    },
+    `gatsby-transformer-json`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: process.env.NODE_ENV !== `production`,
+        fileName: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `./config/typography.js`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-catch-links`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: config.siteTitle,
+        short_name: config.siteTitleShort,
+        description: config.siteDescription,
+        start_url: `/?utm_source=a2hs`,
+        background_color: config.manifest.backgroundColor,
+        theme_color: config.manifest.themeColor,
+        display: `standalone`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline/
+    // `gatsby-plugin-offline`,
+  ],
+}
